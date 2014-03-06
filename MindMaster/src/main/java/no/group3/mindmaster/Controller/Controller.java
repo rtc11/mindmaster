934358c472159e9a4a9f3d33e2d479c10fd96bb6
Wilhelm W Schive <wilumium@hotmail.com>
2014-03-06 15:11:56 +0100

    last attributes and methods from classdiagram

--- MindMaster/src/main/java/no/group3/mindmaster/Controller/Controller.java
+++ MindMaster/src/main/java/no/group3/mindmaster/Controller/Controller.java
@@ -1,7 +1,24 @@
 package no.group3.mindmaster.Controller;
 
+import java.beans.PropertyChangeEvent;
+
+import no.group3.mindmaster.Model.Model;
+
 /**
  * Created by Wschive on 06/03/14.
  */
 public class Controller {
+
+    //Connection con
+    Model model;
+    //View view;
+    //EventHandler eventHandler;
+
+    public void propertyChange(PropertyChangeEvent propChange){
+        
+    }
+
+
+
+
 }
--- MindMaster/src/main/java/no/group3/mindmaster/Model/Model.java
+++ MindMaster/src/main/java/no/group3/mindmaster/Model/Model.java
@@ -1,7 +1,19 @@
 package no.group3.mindmaster.Model;
 
+import java.beans.PropertyChangeSupport;
+import java.util.ArrayList;
+
 /**
  * Created by Wschive on 06/03/14.
  */
 public class Model {
+    ArrayList<ColorPegSequence> history;
+    ArrayList<KeyPeg> keyPegs;
+    ColorPegSolutionSequence solution;
+    PropertyChangeSupport propChange;
+
+
+    public void addToHistory(ColorPegSequence sequence){
+
+    }
 }
