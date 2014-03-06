b11a93875b399d65a2626a598ea3d675040af59b
Wilhelm W Schive <wilumium@hotmail.com>
2014-03-06 14:19:24 +0100

    created class files and packaged them

---
+++ MindMaster/src/main/java/no/group3/mindmaster/Controller/Controller.java
@@ -0,0 +1,7 @@
+package no.group3.mindmaster.Controller;
+
+/**
+ * Created by Wschive on 06/03/14.
+ */
+public class Controller {
+}
---
+++ MindMaster/src/main/java/no/group3/mindmaster/Model/KeyPeg.java
@@ -0,0 +1,7 @@
+package no.group3.mindmaster.Model;
+
+/**
+ * Created by Wschive on 06/03/14.
+ */
+public enum KeyPeg {
+}
