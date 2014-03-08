7b1b007ef09235f93429410a9ea244ca20c46378
janzh <erik_197@hotmail.com>
2014-03-08 18:49:23 +0100

    testconnection

--- MindMaster/src/main/java/no/group3/mindmaster/MainActivity.java
+++ MindMaster/src/main/java/no/group3/mindmaster/MainActivity.java
@@ -18,6 +18,7 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         if (savedInstanceState == null) {
             getFragmentManager().beginTransaction()
                     .add(R.id.container, new PlaceholderFragment())
