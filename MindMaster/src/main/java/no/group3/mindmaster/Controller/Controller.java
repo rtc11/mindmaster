7a6e3ace4bee08988fa93f11092c9e82a1924af8
JeppeE <jeppeer@gmail.com>
2014-03-10 14:41:30 +0100

    Implemented PropertyChangeListener in Controller.java and propertyChange method.

--- MindMaster/src/main/java/no/group3/mindmaster/Controller/Controller.java
+++ MindMaster/src/main/java/no/group3/mindmaster/Controller/Controller.java
@@ -1,24 +1,47 @@
 package no.group3.mindmaster.Controller;
 
 import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
+import no.group3.mindmaster.Model.ColorPegSequence;
 import no.group3.mindmaster.Model.Model;
+import no.group3.mindmaster.Network.Connection;
 
 /**
  * Created by Wschive on 06/03/14.
  */
-public class Controller {
 
-    //Connection con
+public class Controller implements PropertyChangeListener{
+
+    Connection connection;
     Model model;
     //View view;
     //EventHandler eventHandler;
 
-    public void propertyChange(PropertyChangeEvent propChange){
-        
+    private ArrayList<ColorPegSequence> oldHistory;
+    private ArrayList<ColorPegSequence> currentHistory;
+
+    public Controller() {
+        oldHistory = new ArrayList<ColorPegSequence>();
+        currentHistory = new ArrayList<ColorPegSequence>();
     }
 
+    public ArrayList<ColorPegSequence> getOldHistory() {
+        return oldHistory;
+    }
 
+    public ArrayList<ColorPegSequence> getCurrentHistory() {
+        return currentHistory;
+    }
 
-
+    @SuppressWarnings("unchecked")
+    @Override
+    public void propertyChange(PropertyChangeEvent pcs){
+        //Name of the property that has changed.
+        String changedProperty = pcs.getPropertyName();
+        oldHistory = (ArrayList<ColorPegSequence>) pcs.getOldValue();
+        currentHistory = (ArrayList<ColorPegSequence>) pcs.getNewValue();
+    }
 }
