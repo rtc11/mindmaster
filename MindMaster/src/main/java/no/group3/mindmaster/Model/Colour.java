393125f059cc1b24500287da4fc6f84d0bc97ac2
Wilhelm W Schive <wilumium@hotmail.com>
2014-03-10 14:06:26 +0100

    file not previously added from last commit

---
+++ MindMaster/src/main/java/no/group3/mindmaster/Model/Colour.java
@@ -0,0 +1,20 @@
+package no.group3.mindmaster.Model;
+
+
+import android.graphics.Color;
+
+/**
+ * Created by Wschive on 06/03/14.
+ * Each enum contains an android.graphics.Color int. Red is therefore RED(Color.RED).
+ */
+
+
+public enum Colour {
+    RED (Color.RED), GREEN(Color.GREEN), YELLOW(Color.YELLOW), BLUE(Color.BLUE), MAGENTA(Color.MAGENTA), CYAN(Color.CYAN);
+
+    private final int color;
+    Colour(int color){
+        this.color = color;
+    }
+
+}
