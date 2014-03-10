348dffbc1ca1ae8fb385776e88e60801e02680f7
Wilhelm W Schive <wilumium@hotmail.com>
2014-03-10 13:06:53 +0100

    colours enum is now filled out

--- MindMaster/src/main/java/no/group3/mindmaster/Model/Colours.java
+++ MindMaster/src/main/java/no/group3/mindmaster/Model/Colours.java
@@ -1,9 +1,20 @@
 package no.group3.mindmaster.Model;
 
 
+import android.graphics.Color;
+
 /**
  * Created by Wschive on 06/03/14.
+ * Each enum contains an android.graphics.Color int. Red is therefore RED(Color.RED).
  */
+
+
 public enum Colours {
-    RED, GREEN, YELLOW, BLUE
+    RED (Color.RED), GREEN(Color.GREEN), YELLOW(Color.YELLOW), BLUE(Color.BLUE), MAGENTA(Color.MAGENTA), CYAN(Color.CYAN);
+
+    private final int color;
+    Colours(int color){
+        this.color = color;
+    }
+    
 }
