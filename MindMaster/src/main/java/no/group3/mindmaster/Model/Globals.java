92564e0ff92d56f3ec875592bf35eab441af6eb0
Wilhelm W Schive <wilumium@hotmail.com>
2014-03-13 12:57:00 +0100

    created globals

---
+++ MindMaster/src/main/java/no/group3/mindmaster/Model/Globals.java
@@ -0,0 +1,9 @@
+package no.group3.mindmaster.Model;
+
+/**
+ * Created by Wschive on 13/03/14.
+ */
+public class Globals {
+    public static int sequenceLength = 4;
+    public static int numberOfColours = Colour.values().length;
+}
