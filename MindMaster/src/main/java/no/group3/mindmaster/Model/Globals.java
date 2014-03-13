69531ceb0ac5a1741914692e6a6c502892f1d681
Wilhelm W Schive <wilumium@hotmail.com>
2014-03-13 13:01:21 +0100

    implemented globals

--- MindMaster/src/main/java/no/group3/mindmaster/Model/Globals.java
+++ MindMaster/src/main/java/no/group3/mindmaster/Model/Globals.java
@@ -4,6 +4,6 @@ package no.group3.mindmaster.Model;
  * Created by Wschive on 13/03/14.
  */
 public class Globals {
-    public static int sequenceLength = 4;
-    public static int numberOfColours = Colour.values().length;
+    public static int SEQUENCELENGTH = 4;
+    public static int NUMBEROFCOLOURS = Colour.values().length;
 }
