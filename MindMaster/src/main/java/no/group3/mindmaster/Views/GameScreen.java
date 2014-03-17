6e2c613b78f36f68ac53e29c44d1aff5ab65b6c4
janzh <erik_197@hotmail.com>
2014-03-17 17:05:49 +0100

    Added adapter functionality that does not work, with a layour for rows in spinner

--- MindMaster/src/main/java/no/group3/mindmaster/Views/GameScreen.java
+++ MindMaster/src/main/java/no/group3/mindmaster/Views/GameScreen.java
@@ -6,13 +6,17 @@ package no.group3.mindmaster.Views;
 
 import java.util.ArrayList;
 import android.app.Fragment;
+import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import no.group3.mindmaster.Model.ColorPeg;
@@ -25,12 +29,17 @@ public class GameScreen extends Fragment {
     // TODO: Change object type in ArrayList to the type of the drawn Peg-object
     private String TAG = "MindMaster.GameScreen";
 
-    private ArrayList<ColorPeg> pegsList;
-    private ArrayList<KeyPeg> keyPegs;
+    // Images for pegs
+    int arr_images[] = { R.drawable.blue,
+            R.drawable.green, R.drawable.orange,
+            R.drawable.purple, R.drawable.red, R.drawable.yellow};
+
     private ArrayList<Spinner> spinnerList;
     private View rootView;
 
     private Controller controller;
+    private LayoutInflater inflater;
+
     public GameScreen() {
         controller = Controller.getControllerInstance();
         controller.newSoloGame();
@@ -49,6 +58,9 @@ public class GameScreen extends Fragment {
 
     private void placePegsInSpinners(){
         initializeSpinners();
+
+        Spinner mySpinner = (Spinner)rootView.findViewById(R.id.spinner1);
+        mySpinner.setAdapter(new SpinnerAdapter(getActivity(), R.layout.spinner_row));
     }
 
     private void initializeSpinners(){
@@ -57,13 +69,43 @@ public class GameScreen extends Fragment {
         spinnerList.add((Spinner) rootView.findViewById(R.id.spinner2));
         spinnerList.add((Spinner) rootView.findViewById(R.id.spinner3));
         spinnerList.add((Spinner) rootView.findViewById(R.id.spinner4));
+
     }
 
     @Override
     public View onCreateView (LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        this.inflater = inflater;
         rootView = inflater.inflate(R.layout.game_screen, container, false);
         placePegsInSpinners();
         Log.d(TAG, "Spinner list created");
         return rootView;
+    }
+
+    public class SpinnerAdapter extends ArrayAdapter<String> {
+
+        public SpinnerAdapter(Context context, int textViewResourceId) {
+            super(context, textViewResourceId);
+        }
+
+        @Override
+        public View getDropDownView(int position, View convertView,ViewGroup parent) {
+            return getCustomView(position, convertView, parent);
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            return getCustomView(position, convertView, parent);
+        }
+
+        public View getCustomView(int position, View convertView, ViewGroup parent) {
+
+            LayoutInflater inflater = LayoutInflater.from(getContext());
+            View row = inflater.inflate(R.layout.spinner_row, parent, false);
+
+            ImageView icon = (ImageView)row.findViewById(R.id.image);
+            icon.setImageResource(arr_images[position]);
+
+            return row;
+        }
     }
 }
